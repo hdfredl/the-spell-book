199e928dab7b9919c4ee5c00b367131b528b19f8
hdfredl <dat.leef@gmail.com>
2024-05-17 10:33:01 +0200

    finished

--- src/components/Spell.jsx
+++ src/components/Spell.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function Spell() {
-    return (  <>
-    
+function Spell(props) {
+
+
+    return (  
+    <>
+    <h2>{props.spell.name}</h2>
+        <button onClick={() => props.onCastSpell(props.spell)} >Cast spell</button> 
+        <br />
+        <hr />
+
     </>);
 }
 
--- src/components/Spellbook.jsx
+++ src/components/Spellbook.jsx
@@ -4,28 +4,56 @@ import Spell from './Spell';
 function Spellbook() {
     
     const [spells, setSpells] = useState([]);
+    const [castedSpell, setCastedSpell] = useState([]);
 
     useEffect(() => {
-        // Hämta spells
+        console.log("Spellbook created.....");
+        //Hämta spells och sätt spells-variabeln.
         fetch("http://localhost:3000/spells")
-        .then((response) => response.json())
-        .then((data) => setSpells(data));
-
-    });
+          .then((response) => response.json())
+          .then((data) => setSpells(data));
+      }, []);
 
     useEffect(() => {
         if(spells.length > 0)
-        console.log("SPELLS HAVE BEEN SET"), [spells]
-        });
+        {console.log("SPELLS HAVE BEEN SET")}
+        
+        },  [spells]);
+
+        function handleOnCastSpell(spell) {
+            console.log("casting spell...", spell.name)
+            setCastedSpell(spell); // set castedSpell så den use-state på varje prop
+            
+            if(spell.oneUse )
+            {
+                console.log("spell can be casted once")
+                // Ta bort spellen från spell listan
+                // const oneUseSpell = spell.filter((s) => s.name !== spell.name)
+                // setCastedSpell(oneUseSpell)
+
+                // Skapa en egen frikopplad spells arrayen
+                let tempSpells = [...spells]
+                // filtrera ut spellen som vi klickat på
+                tempSpells = tempSpells.filter ((s) => s.id !== spell.id);
+
+                // Lägg in tempSpells i setSpells (spells)
+                setSpells(tempSpells)
+            }
+        }
     
     
     return (  
         <>
+        <h1>{castedSpell.description}</h1>
         {spells.map((s) => (    
-            <h1>{spells.name}</h1>
+           <Spell 
+           key = {s.id}
+           spell = {s} 
+           onCastSpell = { handleOnCastSpell} // hämtar onCastSpell från Spell.jsx - > {() => handleOnCastSpell} 
 
+           />
         ))}
-        
+
         </>
     );
 
